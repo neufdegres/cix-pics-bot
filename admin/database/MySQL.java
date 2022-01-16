f630286930f5eb714d4d19a009323ebd561d0a5e
vicky <77degres@gmail.com>
2022-01-16 23:19:27 +0100

    (voir commit precedent)

--- admin/database/MySQL.java
+++
@@ -1,8 +0,0 @@
-package database;
-
-public class MySQL {
-    public static final String DB_URL = "jdbc:mysql://51.38.225.23:3306/picture_bot";
-    public static final String USER = "vicky";
-    public static final String PASS = "adminBOT77degres";
-    public static final String QUERY = "SELECT * FROM members";
-}
